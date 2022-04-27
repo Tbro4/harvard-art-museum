7ecaf9736ba73a289efe04c264e7cfc9c978effa
Tyler Brooks <thetylerbrooks@gmail.com>
2022-04-27 17:36:14 -0600

    initial fetch request working

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
-//access random object
+var APIkey = "a019a706-259c-4a2f-9933-1f3c5d716d8a";
 
 //https://api.harvardartmuseums.org/object?sort=random&apikey=a019a706-259c-4a2f-9933-1f3c5d716d8a
 
-//this will be the filler of techniques for the URL endpoint. Each technique needs a | between it
+//values from each checked button
 var techniqueChoices = [];
+//string values together to place in URL endpoint
+var techniques;
 
 //upon submit, any clicked buttons value is pushed to techniqueChoices array
+//needs to hide div, make API call, show new div with pics
 document.querySelector("#submitBtn").addEventListener("click", function (e) {
   e.preventDefault();
   var form = document.querySelector("#buttonGroup");
@@ -14,5 +17,21 @@ document.querySelector("#submitBtn").addEventListener("click", function (e) {
       techniqueChoices.push(inp.value);
     }
   });
-  console.log(techniqueChoices);
+  //string array values with | seperator
+  techniques = techniqueChoices.join("|");
+
+  var museumURL =
+    "https://api.harvardartmuseums.org/object?technique=" +
+    techniques +
+    "&sort=random&size=100&apikey=" +
+    APIkey;
+
+  fetch(museumURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.records[0].id);
+    });
 });
