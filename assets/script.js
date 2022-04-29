40693f1bc932b5006d20743fb1664f116334ca07
Tyler Brooks <thetylerbrooks@gmail.com>
2022-04-28 22:50:41 -0600

    Clicking a favorite pic takes you to a new div with selected picture and pic info

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 var APIkey = "a019a706-259c-4a2f-9933-1f3c5d716d8a";
-
-//https://api.harvardartmuseums.org/object?sort=random&apikey=a019a706-259c-4a2f-9933-1f3c5d716d8a
-
 //values from each checked button
 var techniqueChoices = [];
 //string values together to place in URL endpoint
@@ -9,8 +6,84 @@ var techniques;
 //holds id #s to dynamically generate pics
 var ids = [];
 
+var idCall;
+
+//reference to the pics container for button event bubbling (cannot listen for dynamically created buttons without this)
+var picsContainer = document.querySelector(".pics");
+
+//listen for favBtn click
+picsContainer.addEventListener("click", function (e) {
+  //check if we are clicking a Favorite button, then do something
+  if (e.target.classList.contains("favBtn")) {
+    // console.log(e.target);
+    //hide pics section
+    $(".pics").addClass("hidden");
+
+    //fetching endpoint using targets ID and appending to picInfo container
+    objectID = e.target.getAttribute("data-id");
+    // console.log(objectID);
+    var objectURL =
+      "https://api.harvardartmuseums.org/object/" +
+      objectID +
+      "?apikey=" +
+      APIkey;
+
+    fetch(objectURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+
+        //dynamically create elements
+        var img = $("<img>");
+        var title = $("<h2>");
+        var century = $("<h4>");
+        var period = $("<h4>");
+        var division = $("<h4>");
+        var technique = $("<h4>");
+        var medium = $("<h4>");
+
+        img.attr({
+          src: data.primaryimageurl,
+          class: "img-responsive m-1",
+          width: "100%",
+          height: "auto",
+        });
+        //use if statements to keep null values from appearing
+        if (data.title !== null) {
+          title.text(data.title);
+        }
+        if (data.century !== null) {
+          century.text("Century: " + data.century);
+        }
+        if (data.period !== null) {
+          period.text("Period: " + data.period);
+        }
+        if (data.division !== null) {
+          division.text("Division: " + data.division);
+        }
+        if (data.technique !== null) {
+          technique.text("Technique: " + data.technique);
+        }
+        if (data.medium !== null) {
+          medium.text("Medium: " + data.medium);
+        }
+
+        $(".picInfo").append(
+          title,
+          century,
+          period,
+          division,
+          technique,
+          medium,
+          img
+        );
+      });
+  }
+});
+
 //upon submit, any clicked buttons value is pushed to techniqueChoices array
-//needs to hide div, make API call, show new div with pics
 document.querySelector("#submitBtn").addEventListener("click", function (e) {
   e.preventDefault();
   //hide frontPage
@@ -31,15 +104,12 @@ document.querySelector("#submitBtn").addEventListener("click", function (e) {
     "&sort=random&size=100&apikey=" +
     APIkey;
 
-  //   var picsURL =
-  //     "https://api.harvardartmuseums.org/object/" + id + "?apikey=" + APIkey;
-
   fetch(museumURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      //   console.log(data);
       //looks through each record, checks if it has an image (needed 3 || conditions because some of the records only contain one of them), and if it does, pushes object ID to array until 4 ids have been pushed
       for (i = 0, x = 0; i < 4; i++, x++) {
         if (
@@ -52,8 +122,8 @@ document.querySelector("#submitBtn").addEventListener("click", function (e) {
           ids.push(data.records[x].id);
         }
       }
-      console.log(ids);
-
+      //   console.log(ids);
+      //loop through each id in array and create and append a card to the pic section
       for (i = 0; i < 4; i++) {
         //URL variable used to fetch the specific object data by its ID. Then we will use that data to grab primaryimageurl
         var idCall =
@@ -67,39 +137,35 @@ document.querySelector("#submitBtn").addEventListener("click", function (e) {
             return response.json();
           })
           .then(function (data) {
-            console.log(data);
+            // console.log(data);
             var primaryImageURL = data.primaryimageurl;
-            console.log(primaryImageURL);
-
+            // console.log(primaryImageURL);
+            //make card elements
             var cardDiv = $("<div>");
             var cardBdyDiv = $("<div>");
             var imageLink = $("<a>");
             var image = $("<img>");
-            var button = $("<a>");
-
+            var button = $("<button>");
+            //fill card elements with classes,data,etc
             cardDiv.addClass("card col-lg-6 col-sm-12");
             cardBdyDiv.addClass("card-body");
             imageLink.attr({ href: primaryImageURL, target: "_blank" });
-
             image.attr({
               src: primaryImageURL,
               class: "card-img-top",
               alt: "...",
             });
-
             imageLink.append(image);
 
             button
-              .attr({ href: primaryImageURL, class: "btn btn-primary" })
+              .attr({ class: "btn btn-primary favBtn", "data-id": data.id })
               .text("Favorite");
-
+            //append elements to pics section
             cardBdyDiv.append(button);
-
             cardDiv.append(imageLink, cardBdyDiv);
-
             $(".pics").append(cardDiv);
 
-            //show pics div
+            //show pics section
             $(".pics").removeClass("hidden");
           });
       }
